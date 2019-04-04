fc59fdec24b810c429126c41ba4eead906622c4f
Aluno <aluno@eteclencois.com.br>
2019-04-04 12:26:30 -0300

    Cricao de modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Projeto de Aula';
 
-  categorias = [
-    {id: '1', nome: 'Informatica'},
-    {id: '2', nome: 'Escritorio'},
-    {id: '3', nome: 'Perfumaria'},
-    {id: '4', nome: 'Lazer'}
-  ]
+  
 }
